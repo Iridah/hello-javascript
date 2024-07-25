5c76ac769d592268703b71d7611db65241592c07
Alejandro Silva Quiñones <janosilva@gmail.com>
2024-07-25 17:43:55 -0400

    Update 03-beginner-exercises.js
    completado de ejercicios en el archivo 03

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -6,20 +6,59 @@ https://www.twitch.tv/videos/2200149072?t=00h08m02s
 
 // 1. Escribe un comentario en una línea
 
+console.log("hola mundo!");   //mi primer escrito
+
 // 2. Escribe un comentario en varias líneas
+
+console.log("hola mundo!");   /*"este es un comentario
+                                en varias lineas"*/
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
 
+const myName = "Alejandro";
+const age = 40;
+const isStudent = false;
+const undefValue = "undef";
+const nullValue = null;
+const wolfgard = Symbol("mysumbol");
+const theBigInt= 7569863254125642n;
+
 // 4. Imprime por consola el valor de todas las variables
+
+console.log(myName);
+console.log(age);
+console.log(isStudent);
+console.log(undefValue);
+console.log(nullValue);
+console.log(wolfgard);
+console.log(theBigInt);
 
 // 5. Imprime por consola el tipo de todas las variables
 
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
 
+let myName2 = "Fernando";
+let age2 = 30;
+let isStudent2 = true;
+let undefValue2 = "undef";
+let nullValue2 = null;
+let zakku = Symbol2("mysymbol2");
+let theBigInt2= 7569863254865483265982155452125642n;
+
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
 
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
 
+const myName3 = "Alejandro";
+const age3 = 40;
+const isStudent3 = false;
+const undefValue3 = "undef";
+const nullValue3 = null;
+const wolfgard3 = Symbol("mysumbol");
+const theBigInt3= 7569863254125642n;
+
 // 9. A continuación, modifica los valores de las constantes
+
+/*Las constantes en JS son inmutables, para modificarlas, deben ser cambiadas por "let"*/
 
 // 10. Comenta las líneas que produzcan algún tipo de error al ejecutarser
