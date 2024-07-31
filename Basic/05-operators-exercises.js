22f02dc24a645be68242ca3a1f0a7f6e40a5f0d0
Alejandro Silva Quiñones <janosilva@gmail.com>
2024-07-31 12:53:57 -0400

    Desarrollo modulo 05

--- Basic/05-operators-exercises.js
+++ Basic/05-operators-exercises.js
@@ -5,6 +5,15 @@ https://www.twitch.tv/videos/2200149072?t=00h08m02s
 */
 
 // 1. Crea una variable para cada operación aritmética
+let a = 5;
+let b = 2;
+let sum = a + b;
+let res = b - a;
+let mul = a * b;
+let div = b / a;
+let mod = b % a;
+let exp = a ** b;
+console.log()
 
 // 2. Crea una variable para cada tipo de operación de asignación,
 //    que haga uso de las variables utilizadas para las operaciones aritméticas
@@ -15,11 +24,19 @@ https://www.twitch.tv/videos/2200149072?t=00h08m02s
 
 // 5. Utiliza el operador lógico and
 
+console.log( 5 >= 5 && 2 < 3)
+
 // 6. Utiliza el operador lógico or
+
+console.log( 5 >= 5 || 2 < 3)
 
 // 7. Combina ambos operadores lógicos
 
+console.log( 5 >= 5 && 2 < 3 || 8 < 6)
+
 // 8. Añade alguna negación
+
+console.log(!(2 > 87 && 42 > 321))
 
 // 9. Utiliza el operador ternario
 
